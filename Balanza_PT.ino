1fd03791821b5dd099d45639c96397a1cb20eb3c
EduardoOL27 <al2172004092@azc.uam.mx>
2022-01-17 17:48:06 -0600

    Codigo_Balanzas
    Se añadió el código para contabilizar la cantidad de unidades en la bascula.

--- Balanza_PT.ino
+++ Balanza_PT.ino
@@ -5,26 +5,75 @@
 //Antes de ejecutar el programa la balanza no debe estar cargada con ningún peso.
 
 //Cableado al ESP32CAM
-const int Pin_DT = 4;
-const int Pin_SCK = 2; 
+const int Pin_DT1 = 4;
+const int Pin_SCK1 = 2; 
+const int Pin_DT2 = 14;
+const int Pin_SCK2 = 15;
+const int Pin_DT3 = 13;
+const int Pin_SCK3 = 12;
+
+const int piezas1P;
+const int piezas2P;
+const int interruptor1P = 0.136; //peso por unidad en gramos. 
+const int Interruptor2P = 0.272; //peso por unidad en gramos.
+
+HX711 balanza1;
+HX711 balanza2;
+HX711 balanza3;
 
 //Obtención del factor de conversión para convertir valor de lectura en un valor con unidades de peso.
 
 void setup() {
   Serial.begin(115200);
-  balanza.begin(Pin_DT, Pin_SCK);
-  Serial.print("Lectura del valor del ADC:t");
-  Serial.println(balanza.read());
+  balanza1.begin(Pin_DT1, Pin_SCK1);
+  balanza2.begin(Pin_DT2, Pin_SCK2);
+  balanza3.begin(Pin_DT3, Pin_SCK3);
+  Serial.print("Lectura del valor del ADC: Kg");
+  Serial.println(balanza1.read());
+  Serial.println(balanza2.read());
+  Serial.println(balanza3.read());
   Serial.println("No ponga ningún objeto sobre la balanza");
   Serial.println("Destarando...");
-  balanza.set_scale(335597.8); //La escala por defecto es 1
-  balanza.tare(20);  //El peso actual es considerado Tara.
+  balanza1.set_scale(243196.7814); //La escala por defecto es 1
+  balanza2.set_scale(276791.0794); //La escala por defecto es 1
+  balanza3.set_scale(); //La escala por defecto es 1
+  balanza1.tare(20);  //El peso actual es considerado Tara.
+  balanza2.tare(20);  //El peso actual es considerado Tara.
+  balanza3.tare(20);  //El peso actual es considerado Tara.
   Serial.println("Coloque sobre la balanza un peso conocido:");
 }
 
 void loop() {
 
-  Serial.print("Valor de lectura: Kg");
-  Serial.println(balanza.get_value(10),0);
-  delay(100);
+if (balanza1.wait_ready_timeout(1000)) {
+    float peso1 = balanza1.get_units(20);
+    piezas1P = peso1 / interruptor1P;
+      Serial.println("La bascula 2 tiene: ");
+      Serial.print(piezas1P);
+      Serial.println(" Unidades");
+} else {
+    Serial.println("HX711 no encontrado.");
+    delay(2000);
+}
+  
+if (balanza1.wait_ready_timeout(1000)) {
+    float peso2 = balanza2.get_units(20);
+    piezas2P = peso2 / interruptor2P;
+      Serial.println("La bascula 2 tiene: ");
+      Serial.print(piezas2P);
+      Serial.println(" Unidades");
+} else {
+    Serial.println("HX711 no encontrado.");
+    delay(2000);
+}
+
+// (loadcell.wait_ready_timeout(1000)) {
+    //long peso3 = balanza3.get_units(20),3);
+    //Serial.print("Valor de lectura de la balanza 3 es:");
+    //Serial.println(peso3, 3);
+    //Serial.println(" Kg");
+//} else {
+    //Serial.println("HX711 no encontrado.");
+    //delay(2000);
+//}
 }
